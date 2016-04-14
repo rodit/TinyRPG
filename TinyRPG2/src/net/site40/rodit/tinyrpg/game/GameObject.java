59e7a8aa61c4299db4a622387854b5d634390f7b
rodit <rodit9@gmail.com>
2016-04-14 20:30:48 +0100

    Added support for concurrent addition and removal of paint mixers.

--- TinyRPG2/src/net/site40/rodit/tinyrpg/game/GameObject.java
+++ TinyRPG2/src/net/site40/rodit/tinyrpg/game/GameObject.java
@@ -7,40 +7,61 @@ import android.graphics.Paint;
 
 public abstract class GameObject implements IGameObject{
 
+	private ArrayList<IPaintMixer> addQueue;
+	private ArrayList<IPaintMixer> removeQueue;
+
 	protected Paint paint;
 	private ArrayList<IPaintMixer> mixers;
-	
+
 	public GameObject(){
+		this.addQueue = new ArrayList<IPaintMixer>();
+		this.removeQueue = new ArrayList<IPaintMixer>();
+
 		this.mixers = new ArrayList<IPaintMixer>();
 		this.paint = Game.getDefaultPaint();
 	}
-	
+
 	public Paint getPaint(){
 		return paint;
 	}
-	
+
 	public void setPaint(Paint paint){
 		this.paint = paint;
 	}
-	
+
 	public ArrayList<IPaintMixer> getMixers(){
 		return mixers;
 	}
-	
+
 	public void attachPaintMixer(IPaintMixer mixer){
-		if(!mixers.contains(mixer))
-			mixers.add(mixer);
+		synchronized(addQueue){
+			addQueue.add(mixer);
+		}
 	}
-	
+
 	public void detachPaintMixer(IPaintMixer mixer){
-		mixers.remove(mixer);
+		synchronized(removeQueue){
+			removeQueue.add(mixer);
+		}
 	}
-	
+
 	public void preRender(Game game, Canvas canvas){
+		synchronized(addQueue){
+			for(IPaintMixer mixer : addQueue)
+				if(!mixers.contains(mixer))
+					mixers.add(mixer);
+			addQueue.clear();
+		}
+		synchronized(removeQueue){
+			for(IPaintMixer mixer : removeQueue)
+				mixers.remove(mixer);
+			removeQueue.clear();
+		}
+
 		for(IPaintMixer mixer : mixers)
 			mixer.preRender(game, canvas, this);
 	}
-	
+
 	public void postRender(Game game, Canvas canvas){
 		for(IPaintMixer mixer : mixers)
 			mixer.postRender(game, canvas, this);
