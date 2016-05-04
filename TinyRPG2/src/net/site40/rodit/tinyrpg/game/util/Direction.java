957fc4678251491bfba5898c2e8a84611f6a6692
rodit <rodit9@gmail.com>
2016-05-04 21:04:20 +0100

    Added method to get opposite direction.

--- TinyRPG2/src/net/site40/rodit/tinyrpg/game/util/Direction.java
+++ TinyRPG2/src/net/site40/rodit/tinyrpg/game/util/Direction.java
@@ -2,5 +2,20 @@ package net.site40.rodit.tinyrpg.game.util;
 
 public enum Direction {
 
-	D_UP, D_DOWN, D_LEFT, D_RIGHT
+	D_UP, D_DOWN, D_LEFT, D_RIGHT;
+
+	public static Direction opposite(Direction d){
+		switch(d){
+		case D_UP:
+			return D_DOWN;
+		case D_DOWN:
+			return D_UP;
+		case D_LEFT:
+			return D_RIGHT;
+		case D_RIGHT:
+			return D_LEFT;
+		default:
+			return d;
+		}
+	}
 }
