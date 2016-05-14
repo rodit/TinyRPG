9a2cc0d310b3bd8cd791cec80e540e5800294637
rodit <rodit9@gmail.com>
2016-05-14 13:30:39 +0100

    Fixed disposal of map when loading another one.

--- TinyRPG2/src/net/site40/rodit/tinyrpg/game/map/RPGMap.java
+++ TinyRPG2/src/net/site40/rodit/tinyrpg/game/map/RPGMap.java
@@ -9,7 +9,7 @@ import android.graphics.RectF;
 import android.util.Log;
 
 public class RPGMap {
-	
+
 	private String file;
 	private Bitmap background;
 	private Bitmap renderOnTop;
@@ -17,9 +17,9 @@ public class RPGMap {
 	private ArrayList<MapObject> objects;
 	private HashMap<String, ArrayList<MapObject>> objectGroupCache;
 	private HashMap<Region, ArrayList<RectF>> regions;
-	
+
 	private boolean loaded = false;
-	
+
 	public RPGMap(String file, boolean loaded){
 		this.file = file;
 		this.loaded = loaded;
@@ -30,40 +30,42 @@ public class RPGMap {
 		this.objectGroupCache = new HashMap<String, ArrayList<MapObject>>();
 		this.regions = new HashMap<Region, ArrayList<RectF>>();
 	}
-	
+
 	public boolean isLoaded(){
 		return loaded;
 	}
-	
+
 	public String getFile(){
 		return file;
 	}
-	
+
 	public Bitmap getBackground(){
 		return background;
 	}
-	
+
 	public void setBackground(Bitmap background){
 		this.background = background;
 	}
-	
+
 	public Bitmap getRenderOnTop(){
 		return renderOnTop;
 	}
-	
+
 	public void setRenderOnTop(Bitmap renderOnTop){
 		this.renderOnTop = renderOnTop;
 	}
-	
+
 	public MapProperties getProperties(){
 		return properties;
 	}
-	
+
 	public ArrayList<MapObject> getObjects(){
 		return objects;
 	}
-	
+
 	public ArrayList<MapObject> getObjects(String group){
+		if(objectGroupCache == null)
+			objectGroupCache = new HashMap<String, ArrayList<MapObject>>();
 		ArrayList<MapObject> grouped = objectGroupCache.get(group);
 		if(grouped == null){
 			grouped = new ArrayList<MapObject>();
@@ -74,11 +76,11 @@ public class RPGMap {
 		}
 		return grouped;
 	}
-	
+
 	public HashMap<Region, ArrayList<RectF>> getRegions(){
 		return regions;
 	}
-	
+
 	public void addRegionLocation(Region region, RectF location){
 		ArrayList<RectF> locations = regions.get(region);
 		if(locations == null)
@@ -87,11 +89,13 @@ public class RPGMap {
 			locations.add(location);
 		regions.put(region, locations);
 	}
-	
+
 	public void dispose(ResourceManager resources){
 		Log.i("RPGMap", "Disposing of map " + file + ".");
-		background.recycle();
-		renderOnTop.recycle();
+		if(background != null)
+			background.recycle();
+		if(renderOnTop != null)
+			renderOnTop.recycle();
 		background = renderOnTop = null;
 		objects.clear();
 		objects = null;
