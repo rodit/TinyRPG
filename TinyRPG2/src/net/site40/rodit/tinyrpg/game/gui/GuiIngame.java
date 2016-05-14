7f17df8b55d70bb61a60ab17b5ebfadfaff30c41
rodit <rodit9@gmail.com>
2016-05-14 13:24:55 +0100

    Added and removed debug button.

--- TinyRPG2/src/net/site40/rodit/tinyrpg/game/gui/GuiIngame.java
+++ TinyRPG2/src/net/site40/rodit/tinyrpg/game/gui/GuiIngame.java
@@ -1,11 +1,16 @@
 package net.site40.rodit.tinyrpg.game.gui;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 
 import net.site40.rodit.tinyrpg.game.Game;
+import net.site40.rodit.tinyrpg.game.IGameObject;
 import net.site40.rodit.tinyrpg.game.Input;
 import net.site40.rodit.tinyrpg.game.Values;
 import net.site40.rodit.tinyrpg.game.effect.Effect;
+import net.site40.rodit.tinyrpg.game.gui.ComponentListener.ComponentListenerImpl;
+import net.site40.rodit.tinyrpg.game.render.Sprite;
 import net.site40.rodit.util.ColorGradient;
 import android.graphics.Canvas;
 import android.graphics.Color;
@@ -14,7 +19,7 @@ import android.graphics.Paint.Style;
 import android.graphics.RectF;
 
 public class GuiIngame extends Gui{
-	
+
 	public static final float HEALTH_BAR_OFFSET_X = 132;
 	public static final float HEALTH_BAR_OFFSET_Y = 10;
 	public static final float HEALTH_BAR_WIDTH = 16;
@@ -65,7 +70,7 @@ public class GuiIngame extends Gui{
 		btnMenu.setWidth(92);
 		btnMenu.setHeight(92);
 		add(btnMenu);
-		
+
 		ControlButton btnAction = new ControlButton(Input.KEY_ACTION, "btnAction", false);
 		btnAction.setBackgroundDefault("controls/action.png");
 		btnAction.setX(1280 - 92 * 2);
@@ -73,7 +78,7 @@ public class GuiIngame extends Gui{
 		btnAction.setWidth(92);
 		btnAction.setHeight(92);
 		add(btnAction);
-		
+
 		final ColorGradient healthTextGradient = new ColorGradient(255, 255, 255, 255, 0, 0);
 		final ColorGradient magikaTextGradient = healthTextGradient;
 		Component playerPanel = new Component(){
@@ -87,40 +92,40 @@ public class GuiIngame extends Gui{
 					if(gui.isActive() && (gui instanceof GuiIngameMenu || gui instanceof GuiPlayerInventory || gui instanceof GuiOptions || gui instanceof GuiQuest))
 						return;
 				}
-				
+
 				paint.setTextAlign(Align.CENTER);
 				paint.setTextSize(Values.FONT_SIZE_SMALL - 4f);
 				paint.setStyle(Style.FILL);
-				
+
 				canvas.drawBitmap(game.getResources().getBitmap("gui/pp/bg.png"), null, getBoundsF(), paint);
-				
+
 				float healthRatio = game.getPlayer().getHealthRatio();
 				RectF healthBounds = new RectF(getX() + 32f, getY() + 40f, getX() + 232f, getY() + 68f);
 				paint.setColor(Color.rgb(180, 0, 0));
 				canvas.drawRect(healthBounds, paint);
-				
+
 				float[] healthCenter = new float[] { healthBounds.centerX(), healthBounds.centerY() + 10 };
-				
+
 				float healthBarWidth = healthBounds.width() * healthRatio;
 				healthBounds.right = healthBounds.left + healthBarWidth;
 				paint.setColor(Color.rgb(0, 170, 0));
 				canvas.drawRect(healthBounds, paint);
 				paint.setColor(healthTextGradient.getColor(healthRatio));
 				canvas.drawText(game.getPlayer().getHealth() + "/" + game.getPlayer().getMaxHealth(), healthCenter[0], healthCenter[1], paint);
-				
+
 				float magikaRatio = game.getPlayer().getMagikaRatio();
 				RectF magikaBounds = new RectF(getX() + 32f, getY() + 80f, getX() + 232f, getY() + 108f);
 				paint.setColor(Color.rgb(0, 0, 120));
 				canvas.drawRect(magikaBounds, paint);
-				
+
 				float[] magikaCenter = new float[] { magikaBounds.centerX(), magikaBounds.centerY() + 10 };
-				
+
 				RectF magikaBoundsFull = new RectF(magikaBounds.left, magikaBounds.top, magikaBounds.left + magikaBounds.width() * magikaRatio, magikaBounds.bottom);
 				paint.setColor(Color.BLUE);
 				canvas.drawRect(magikaBoundsFull, paint);
 				paint.setColor(magikaTextGradient.getColor(magikaRatio));
 				canvas.drawText(game.getPlayer().getMagika() + "/" + game.getPlayer().getMaxMagika(), magikaCenter[0], magikaCenter[1], paint);
-				
+
 				ArrayList<String> drawn = new ArrayList<String>();
 				for(Effect e : game.getPlayer().getEffects()){
 					if(drawn.contains(e.getName()))
@@ -138,7 +143,7 @@ public class GuiIngame extends Gui{
 		playerPanel.setHeight(160f);
 		add(playerPanel);
 	}
-	
+
 	@Override
 	public boolean shouldDrawOverDialog(){
 		return true;
