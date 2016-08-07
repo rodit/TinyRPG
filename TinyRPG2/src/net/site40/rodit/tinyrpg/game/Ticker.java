13de90dd51201587e34ff92709f57853c4c038d1
rodit <rodit9@gmail.com>
2016-08-07 11:31:44 +0100

    Added ticker class to update entities periodically.

---
+++ TinyRPG2/src/net/site40/rodit/tinyrpg/game/Ticker.java
@@ -0,0 +1,37 @@
+package net.site40.rodit.tinyrpg.game;
+
+public class Ticker {
+
+	private long interval;
+	private long lastUpdate;
+	
+	public Ticker(long interval){
+		this.interval = interval;
+		this.lastUpdate = 0l;
+	}
+	
+	public long getInvterval(){
+		return interval;
+	}
+	
+	public void setInterval(long interval){
+		this.interval = interval;
+	}
+	
+	public long getLastUpdate(){
+		return lastUpdate;
+	}
+	
+	public void setLastUpdate(long lastUpdate){
+		this.lastUpdate = lastUpdate;
+	}
+	
+	public boolean shouldRun(Game game){
+		long now = game.getTime();
+		if(now - lastUpdate >= interval){
+			lastUpdate = now;
+			return true;
+		}
+		return false;
+	}
+}
