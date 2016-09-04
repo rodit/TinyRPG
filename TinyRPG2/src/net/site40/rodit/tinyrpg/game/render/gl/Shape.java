4af73316e373d0adff9e23288fc89759767f45d7
rodit <rodit9@gmail.com>
2016-09-04 18:01:36 +0100

    Removed obsolete/old classes.

--- TinyRPG2/src/net/site40/rodit/tinyrpg/game/render/gl/Shape.java
+++
@@ -1,6 +0,0 @@
-package net.site40.rodit.tinyrpg.game.render.gl;
-
-public class Shape {
-	
-	
-}
--- TinyRPG2/src/net/site40/rodit/util/ISavable.java
+++
@@ -1,9 +0,0 @@
-package net.site40.rodit.util;
-
-import java.io.IOException;
-
-public interface ISavable {
-	
-	public void serialize(TinyOutputStream out)throws IOException;
-	public void deserialize(TinyInputStream in)throws IOException;
-}
