ff3844ff8e5e1bd0c27887a803639d494aec70f5
Rodit <rodit9@gmail.com>
2016-10-30 11:10:56 +0000

    Centralised commonly used resources (empty arrays).

---
+++ TinyRPG2/src/net/site40/rodit/tinyrpg/game/render/MM.java
@@ -0,0 +1,7 @@
+package net.site40.rodit.tinyrpg.game.render;
+
+public class MM {
+	
+	public static final String[] EMPTY_STRING_ARRAY = new String[0];
+	public static final Object[] EMPTY_OBJECT_ARRAY = new Object[0];
+}
