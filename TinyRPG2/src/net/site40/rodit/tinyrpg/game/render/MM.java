e7e15edebe901ec0c752570961e1bc171e4c5908
Rodit <rodit9@gmail.com>
2017-02-13 21:58:04 +0000

    Removed obsolete files.

--- TinyRPG2/src/net/site40/rodit/tinyrpg/game/render/MM.java
+++
@@ -1,7 +0,0 @@
-package net.site40.rodit.tinyrpg.game.render;
-
-public class MM {
-	
-	public static final String[] EMPTY_STRING_ARRAY = new String[0];
-	public static final Object[] EMPTY_OBJECT_ARRAY = new Object[0];
-}
